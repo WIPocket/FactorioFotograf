145797f7ef68a78c0f6d618f13637656b5773249
Prokop Randacek <prokop@randacek.dev>
2022-02-25 16:47:24 +0100

    separate stb compilation unit

--- src/img.c
+++ src/img.c
@@ -19,11 +19,8 @@
 
 #define PATH_LEN 256
 
-#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
 extern bool png; // from main.c; if we are working with PNGs instead of JPEGs
---
+++ src/stb.c
@@ -0,0 +1,10 @@
+// This file just holds the STB implementations
+// so that is doesn't have to be recompiled when img.c
+// changes
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
